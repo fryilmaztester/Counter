996c00ca05148c6516bec62bfe69530ded9a0e7a
Fatih YILMAZ <fatih.yilmaz@foreks.com>
2021-07-22 00:23:07 +0300

    Counter

---
+++ app/src/main/java/com/fry/counter/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.fry.counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Abstract Class
+
+        object : CountDownTimer(21000,1000){
+
+            override fun onTick(millisUntilFinished: Long) {
+                textView.text = "Kalan: ${millisUntilFinished / 1000}"
+            }
+
+            override fun onFinish() {
+                textView.text = "Kalan: 0"
+            }
+
+        }.start()
+    }
+}
